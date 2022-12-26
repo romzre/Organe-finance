ff981c6e96c17d64da26525c1f41a38fb45f3216
romzre <albrogio@gmail.com>
2022-12-26 09:34:22 +0400

    UPDATE: ServiceDashboard + CleanController

--- src/Controller/BankAccountController.php
+++ src/Controller/BankAccountController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use App\Service\ServiceBankAccount;
+use App\Service\ServiceDashboard;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
 
 
 class BankAccountController extends AbstractController
@@ -16,26 +17,16 @@ class BankAccountController extends AbstractController
     /**
      * @Route("/bank/account/{BankAccountId}", name="app_bank_account")
      */
-    public function index(string $BankAccountId, ServiceBankAccount $serviceBankAccount): Response
+    public function index( ServiceDashboard $serviceDashboard ,string $BankAccountId, ServiceBankAccount $serviceBankAccount): Response
     {
+        $BankAccountsAndCycleDashboard = $serviceDashboard->getDashboard($this->getUser(), ['BankAccountId' => $BankAccountId] );
+        
+        
         $data = [];
-        // BankAccount
-        $data["BankAccounts"] = $serviceBankAccount->getAllBankAccountActive($this->getUser());
-        $bankAccount = $serviceBankAccount->getAccount($BankAccountId);
-        // Forbidden
-        if ($bankAccount->getUser() !== $this->getUser()) 
-        {
-           throw new AccessDeniedException();
-        }
-
-        // Cycle
-        $cycle = $serviceBankAccount->getActiveCycle($bankAccount);
-        if(!empty($cycle))
-        {
-            $data['cycle'] = $cycle;
-        }
-
-        $data["BankAccount"] = $bankAccount;
+        $data["BankAccounts"] = $BankAccountsAndCycleDashboard['BankAccounts'];
+        $data["BankAccount"] = $BankAccountsAndCycleDashboard['BankAccount'];
+        $data["cycle"] = $BankAccountsAndCycleDashboard['Cycle'];
+        
         return $this->render('bank_account/index.html.twig', $data);
     }
 }
---
+++ src/Service/ServiceDashboard.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Entity\BankAccount;
+use App\Repository\CycleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\BankAccountRepository;
+
+class ServiceDashboard extends AbstractService
+{
+    protected EntityManagerInterface $manager;
+    private BankAccountRepository $BankAccountRepository;
+    private CycleRepository $CycleRepository;
+
+    public function __construct(BankAccountRepository $BankAccountRepository, CycleRepository $CycleRepository , EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+        $this->BankAccountRepository = $BankAccountRepository;
+        $this->CycleRepository = $CycleRepository;
+    }
+
+    public function getActiveBankAccounts(User $user): array
+    {
+        $BankAccounts = [];
+        $BankAccounts = $this->BankAccountRepository->findBy(["user" => $user]);
+
+        return $BankAccounts;
+    }
+
+    public function getDashboard(User $user, array $options)
+    {
+        $data = [];
+        $data['BankAccounts'] =  $this->getActiveBankAccounts($user);
+
+        if(!empty($options['BankAccountId']))
+        {
+            $BankAccount =  $this->getAccount($options['BankAccountId']);
+            $Cycle =  $this->getActiveCycle($BankAccount);
+            
+            $data['BankAccount'] = $BankAccount;
+            $data['Cycle'] = $Cycle;
+        }
+
+
+        return $data;
+    }
+
+}
